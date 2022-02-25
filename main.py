0326fcc37fdb4c9363c40da6c35b19d911705d12
adriana44 <adriana.luntraru@gmail.com>
2022-02-25 12:38:50 +0200

    finished project

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import requests
 import os
 import json
-import re
-
+from tweets_handling import process_tweets
 
 bearer_token = os.environ.get("BEARER_TOKEN")
 
-search_url = "https://api.twitter.com/2/tweets/search/recent"
+# search_url = "https://api.twitter.com/2/tweets/search/recent/"
+# search_url = "https://api.twitter.com/2/users/1389110954923724800/tweets"
 
-# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
-# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-# query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev', 'tweet.fields': 'author_id'}
-query_params = {'query': '(from:SecneurX -is:retweet)',
-                'max_results': 10,
-                'expansions': 'author_id',
+query_params = {'expansions': 'author_id',
                 'tweet.fields': 'entities',
                 'user.fields': 'username'}
 
@@ -28,53 +23,100 @@ def bearer_oauth(r):
     return r
 
 
-def connect_to_endpoint(url, params):
-    response = requests.get(url, auth=bearer_oauth, params=params)
-    print(response.status_code)
+def get_rules():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
+    )
     if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
     return response.json()
 
 
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    print(json.dumps(response.json()))
+
+
+def set_rules(delete):
+    # You can adjust the rules if needed
+    sample_rules = [
+        {"value": "Joker (#Malware OR #Trojan OR #Android OR #CyberSecurity OR #PlayStore) has:links",
+         "tag": "joker tweet"}
+    ]
+    payload = {"add": sample_rules}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload,
+    )
+    if response.status_code != 201:
+        raise Exception(
+            "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    print(json.dumps(response.json()))
+
+
+def get_stream(set):
+    with requests.get(
+            "https://api.twitter.com/2/tweets/search/stream",
+            auth=bearer_oauth,
+            stream=True,
+            params=query_params
+    ) as response:
+        print(response.status_code)
+        if response.status_code != 200:
+            raise Exception(
+                "Cannot get stream (HTTP {}): {}".format(
+                    response.status_code, response.text
+                )
+            )
+        for response_line in response.iter_lines():
+            if response_line:
+                json_response = json.loads(response_line)
+                # print(json.dumps(json_response, indent=4, sort_keys=True))
+                with open("response.json", "w") as outfile:
+                    json.dump(json_response, outfile, indent=4)
+                process_tweets(json_response)
+
+
 def main():
-    json_response = connect_to_endpoint(search_url, query_params)
-    # print(json.dumps(json_response, indent=4, sort_keys=True))
-    with open("response.json", "w") as outfile:
-        json.dump(json_response, outfile, indent=4)
+    rules = get_rules()
+    delete = delete_all_rules(rules)
+    set = set_rules(delete)
+    get_stream(set)
 
-    username = ''
-    urls = []
-    playstore_urls = []
-    joker_mentions_cnt = 0
-    online_apps_cnt = 0
-    offline_apps_cnt = 0
-    hashtags = set()
-
-    playstore_pattern = re.compile(r'^https?://play\.google\.com/store/apps')
-
-    last_tweet = json_response['data'][0]
-
-    for url in last_tweet['entities']['urls']:
-        exp_url = url['expanded_url']
-        urls.append(exp_url)
-        if playstore_pattern.match(exp_url):
-            playstore_urls.append(exp_url)
-
-    for tag in last_tweet['entities']['hashtags']:
-        hashtags.add(tag['tag'])
-
-    for user in json_response['includes']['users']:
-        if user['id'] == last_tweet['author_id']:
-            username = user['username']
-
-    print('username:', username)
-    print('hashtags:', hashtags)
-    print('mentioned urls:', urls)
-    print('playstore urls: ', playstore_urls)
+# def connect_to_endpoint(url):
+#     response = requests.get(url, auth=bearer_oauth)
+#     print(response.status_code)
+#     if response.status_code != 200:
+#         raise Exception(response.status_code, response.text)
+#     return response.json()
 
 
-# Press the green button in the gutter to run the script.
+# def main():
+#     # json_response = connect_to_endpoint(search_url, query_params)
+#     # json_response = connect_to_endpoint(search_url)
+#     # with open("response.json", "w") as outfile:
+#     #     json.dump(json_response, outfile, indent=4)
+
+
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ tweets_handling.py
@@ -0,0 +1,142 @@
+from pymongo import MongoClient
+from google_play_scraper import app
+from google_play_scraper.exceptions import NotFoundError
+from urllib.parse import urlparse, parse_qs
+import re
+import os
+import json
+
+admin_pwd = os.environ.get("ADMIN_PWD")
+client = MongoClient(f'mongodb+srv://admin:{admin_pwd}@cluster0.n6i5h.mongodb.net/?retryWrites=true&w=majority')
+db = client.JokerApps
+
+
+def get_app_data(package_name: str) -> bool:
+    """
+    Extracts data about a playstore app and stores it in the apps collection.
+
+    :param package_name: package name of a playstore app
+    :return: True if the app is online, False otherwise
+    """
+    print(f'*** Processing app {package_name} ***')
+
+    app_doc = {
+        '_id': package_name,
+        'online': False
+    }
+    try:
+        app_details = app(package_name)
+    except NotFoundError:
+        print(package_name, 'is offline')
+    else:
+        app_doc = {
+            '_id': package_name,
+            'app_name': app_details['title'],
+            'dev_name': app_details['developer'],
+            'category': app_details['genre'],
+            'last_updated': app_details['released'],
+            'installs': app_details['installs'],
+            'dev_email': app_details['developerEmail'],
+            'privacy_policy_url': app_details['privacyPolicy'],
+            'online': True
+        }
+        print(package_name, 'is online')
+    db.apps.update_one({'_id': package_name}, {"$set": app_doc}, upsert=True)
+    print(f'*** Finished processing app {package_name} ***')
+    return app_doc['online']
+
+
+def process_one_tweet(tweet: dict, users: list) -> None:
+    """
+    Extracts data about one given tweet and stores it in the twitter_accounts collection.
+
+    :param tweet: a dictionary representing a tweet
+    :param users: a list of dictionaries representing users
+    """
+    print(f'\n*** Processing tweet {tweet["id"]} ***')
+    twitter_accounts = db.twitter_accounts
+
+    acc_id = tweet['author_id']
+
+    account = db.twitter_accounts.find_one({"_id": acc_id})
+    if account is None:
+        apps_cnt = 0
+        online_apps_cnt = 0
+        offline_apps_cnt = 0
+        app_ids = []
+        hashtags = set()
+    else:
+        apps_cnt = account['apps_cnt']
+        online_apps_cnt = account['online_apps_cnt']
+        offline_apps_cnt = account['offline_apps_cnt']
+        app_ids = account['mentioned_apps']
+        hashtags = set(account['hashtags'])
+
+    username = None
+    for user in users:
+        if user['id'] == acc_id:
+            username = user['username']
+            break
+
+    playstore_flag = False
+    playstore_pattern = re.compile(r'^https?://play\.google\.com/store/apps')
+    for url in tweet['entities']['urls']:
+        exp_url = url['expanded_url']
+        if playstore_pattern.match(exp_url):
+            playstore_flag = True
+            parsed_url = urlparse(str(exp_url))
+            app_id = parse_qs(parsed_url.query)['id'][0]
+            if app_id not in app_ids:
+                app_ids.append(app_id)
+                if get_app_data(app_id) is True:
+                    online_apps_cnt += 1
+                else:
+                    offline_apps_cnt += 1
+                apps_cnt += 1
+
+    if playstore_flag is False:
+        print('No playstore link found in tweet.')
+        print(f'*** Finished processing tweet {tweet["id"]} ***')
+        return
+
+    new_hashtags = set([hashtag['tag'] for hashtag in tweet['entities']['hashtags']])
+    hashtags.update(new_hashtags)
+
+    account_doc = {
+        '_id': acc_id,
+        'username': username,
+        'hashtags': list(hashtags),
+        "mentioned_apps": app_ids,
+        'apps_cnt': apps_cnt,
+        'online_apps_cnt': online_apps_cnt,
+        'offline_apps_cnt': offline_apps_cnt
+    }
+
+    twitter_accounts.update_one({'_id': account_doc['_id']}, {"$set": account_doc}, upsert=True)
+    print(f'*** Finished processing tweet {tweet["id"]} ***')
+
+
+def process_tweets(tweets_dict: dict) -> None:
+    """
+    Extracts data about the given tweets and stores it in the twitter_accounts collection.
+
+    :param tweets_dict: a dictionary of tweets
+    """
+    tweets = tweets_dict['data']
+    users = tweets_dict['includes']['users']
+
+    if isinstance(tweets, list):
+        for tweet in tweets:
+            process_one_tweet(tweet, users)
+    else:
+        process_one_tweet(tweets, users)
+
+
+# def main():
+#     with open("response.json", "r") as outfile:
+#         json_dict = json.load(outfile)
+#     process_tweets(json_dict)
+#
+#
+# if __name__ == '__main__':
+#     main()
